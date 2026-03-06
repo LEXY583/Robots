4192b2a6f48e28d34756e02774b71eaf15776723
Lexy <lesyad12082003@gmail.com>
2026-03-06 18:09:41 +0500

    MainApplicationFrame: добавлены методы сохранения (saveWindowsState) и восстановления (restoreWindowsState) состояния окна.

--- src/main/java/gui/MainApplicationFrame.java
+++ src/main/java/gui/MainApplicationFrame.java
@@ -3,23 +3,28 @@ package gui;
 import javax.swing.JDesktopPane;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
 import javax.swing.JMenuBar;
-
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.JOptionPane;
+import java.beans.PropertyVetoException;
 
 import log.Logger;
 
 public class MainApplicationFrame extends JFrame {
+
     private final JDesktopPane desktopPane = new JDesktopPane();
+    private final WindowConfigManager configManager = new WindowConfigManager();
     
     public MainApplicationFrame() {
-        //Make the big window be indented 50 pixels from each edge
-        //of the screen.
+        configManager.loadFromFile(); // загружаем конфигурацию
+
         int inset = 50;        
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         setBounds(inset, inset,
@@ -35,6 +40,8 @@ public class MainApplicationFrame extends JFrame {
         GameWindow gameWindow = new GameWindow();
         gameWindow.setSize(400,  400);
         addWindow(gameWindow);
+
+        restoreWindowsState(); // востанавливаем состояния окон после создания
 
         setJMenuBar(generateMenuBar());
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
@@ -86,7 +93,7 @@ public class MainApplicationFrame extends JFrame {
         }
     }
 
-    public void exitApplication() { // обработка выхода + подтверждение
+    public void exitApplication() { 
         int result = JOptionPane.showConfirmDialog(
             this, // родительское окно
             "Вы хотите выйти из Robots?", 
@@ -96,7 +103,33 @@ public class MainApplicationFrame extends JFrame {
         );
         
         if (result == JOptionPane.YES_OPTION) {
+            saveWindowsState(); // сохраняем состояние окон перед выходом
             System.exit(0);
         }
-    }  
+    } 
+    
+    // метод восстановления состояния окон
+    private void restoreWindowsState() {
+        for (JInternalFrame frame : desktopPane.getAllFrames()) {
+            WindowPosition pos = configManager.getWindowState(frame.getTitle());
+            if (pos != null) {
+                frame.setBounds(pos.x, pos.y, pos.width, pos.height);
+                try {
+                    if (pos.isMaximum) {
+                        frame.setMaximum(true);
+                    } else if (pos.isIcon) {
+                        frame.setIcon(true);
+                    }
+                } catch (PropertyVetoException e) {}
+            }
+        }
+    }
+
+    // метод сохранения состояния окон
+    private void saveWindowsState() {
+        for (JInternalFrame frame : desktopPane.getAllFrames()) {
+            configManager.saveWindowState(frame.getTitle(), frame);
+        }
+        configManager.saveToFile();
+    }
 }
